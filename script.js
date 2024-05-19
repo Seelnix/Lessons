288bd14fb144a81857a68220f8462a508a64a565
Max <158865002+Seelnix@users.noreply.github.com>
2024-05-19 23:02:15 +0300

    data type

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-const str='45158'
+let length = 35;
+let weight = 12.4;
 
-console.log(str.split(""));
+let color = "Black";
+let lastName = "Smith";
+
+let x = true;
+let y = false;
+
+const person = {firstName:"Ellison", lastName:"Walker"};
+
+const cars = ["Fiat", "Volvo", "Honda"];
+
+const date = new Date("2035-08-08");

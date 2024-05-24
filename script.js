976f875666a28f877153eed615e650ea36e5ce1f
Max <158865002+Seelnix@users.noreply.github.com>
2024-05-24 21:37:00 +0300

    add correct and same numbers

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-let userName = prompt("Будь ласка, введіть ваше ім'я:");
+let number = prompt("Будь ласка, введіть тризначне число:");
 
-alert(`Hello, ${userName}! How are you?`);
+let digits = number.toString();
+
+let allSame = digits[0] === digits[1] && digits[1] === digits[2];
+
+let anySame = digits[0] === digits[1] || digits[0] === digits[2] || digits[1] === digits[2];
+
+alert(`Чи правда, що всі цифри однакові? ${allSame ? "Так" : "Ні"}`);
+alert(`Чи є серед цифр цифри однакові? ${anySame ? "Так" : "Ні"}`);

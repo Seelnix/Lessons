4a11e4e74ab24148f7d1ac76016a359251bae642
Max <158865002+Seelnix@users.noreply.github.com>
2024-06-11 22:51:31 +0300

    Prime number or not

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-let userName = prompt("Будь ласка, введіть ваше ім'я:");
+let number = parseInt(prompt("Введіть ціле число: "));
 
-alert(`Hello, ${userName}! How are you?`);
+function isPrime(num) {
+  if (num <= 1) {
+    return false; // Числа, менші або рівні 1, не є простими
+  }
+  
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) {
+      return false; // Якщо знайдений дільник, то число не є простим
+    }
+  }
+  
+  return true; // Якщо жодного дільника не знайдено, число є простим
+}
+
+if (isPrime(number)) {
+  console.log(`${number} є простим числом`);
+} else {
+  console.log(`${number} не є простим числом`);
+}

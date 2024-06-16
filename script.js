22a8a28cbfb9f645d2eb6f1d0b1a2e61af3fe40a
Seelnix <merlokc@gmail.com>
2024-06-16 20:20:56 +0300

    Determination of the arithmetic mean

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-let exchangeRate = 26;
-let result = "";
+function averageOfNumbers(arr) {
+    let sum = 0;
+    let count = 0;
 
-for (let dollars = 10; dollars <= 100; dollars += 10) {
-    let costInHryvnias = dollars * exchangeRate;
-    result += `Вартість ${dollars} доларів: ${costInHryvnias} гривень\n`;
+    for (let element of arr) {
+        if (typeof element === 'number') {
+            sum += element;
+            count++;
+        }
+    }
+
+    if (count === 0) {
+        return 0;
+    }
+
+    return sum / count;
 }
 
-console.log(result);
+let mixedArray = [1, 'hello', 3, true, 5, null, 7, 'world'];
+let average = averageOfNumbers(mixedArray);
+console.log(average);
